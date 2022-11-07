e8399c19f8184c3305d3d9a15dfe01ca03902caf
Mateusz Pniewski <smazysiestekjuz@gmail.com>
2022-11-07 10:09:03 +0100

    Profit on entry/exit time

--- main.cpp
+++ main.cpp
@@ -13,38 +13,40 @@ int main()
     cout << "Ile liczb w tablicy: ";
     cin>>ile;
 
+    //dynamiczna alokacja tablicy
     int *tablica;
-    tablica = new int [ile];
+    tablica=new int [ile];
 
-    start=clock();
-
+    //zacznij odliczac czas
+    start = clock();
+    //wczytywanie liczb do tablicy
     for (int i=0; i<ile; i++)
     {
         tablica[i]=i;
         tablica[i]+=50;
     }
-
-    stop=clock();
-    czas=(double)(stop - start) / CLOCKS_PER_SEC;
-    cout<<"czas zapisu (bez wskaznika): "<<czas<<endl;
+    stop = clock();
+    czas = (double)(stop - start) / CLOCKS_PER_SEC;
+    cout<<"Czas zapisu (bez wskaznika): "<<czas<<" s"<<endl;
 
     delete [] tablica;
 
-    int *wskaznik = tablica;
-    tablica = new int[ile];
+    //ponowna alokacja tablicy
+     tablica=new int [ile];
+	 int *wskaznik=tablica;
 
+    //zacznij odliczac czas
     start = clock();
-
+    //wczytywanie liczb do tablicy
     for (int i=0; i<ile; i++)
     {
         *wskaznik = i;
         *wskaznik+=50;
-         wskaznik++;
+        wskaznik++;
     }
-
-    stop=clock();
-    czas=(double)(stop - start) / CLOCKS_PER_SEC;
-    cout<<"czas zapisu (ze wskaznika): "<<czas<<endl;
+    stop = clock();
+    czas = (double)(stop - start) / CLOCKS_PER_SEC;
+    cout<<"Czas zapisu (ze wskaznikiem): "<<czas<<" s";
 
     delete [] tablica;
 
